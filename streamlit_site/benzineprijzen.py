b8242d023e0ab948f1fb54725cac0c312281fa12
Max <m.s.oorschot@live.nl>
2026-01-28 15:37:58 +0100

    removed duplicate code in charts.py

--- streamlit_site/benzineprijzen.py
+++ streamlit_site/benzineprijzen.py
@@ -15,10 +15,10 @@ st.header("Gemiddelde, minimale en maximale benzine prijs per soort tankstation"
 with st.container(border=True):
 
     st.subheader("Benzine prijzen bij budget tankstations:")
-    st.altair_chart(charts.bgt_gas_chart(), use_container_width=True)
+    st.altair_chart(charts.min_max_avg_chart(f_type="benzine", p_type="budget", min_scale=1.70, max_scale=2.40), use_container_width=True)
 
 with st.container(border=True):
     st.subheader("Benzine prijzen bij premium tankstations:")
-    st.altair_chart(charts.prem_gas_chart(), use_container_width=True) # To change to premium gasstations.
+    st.altair_chart(charts.min_max_avg_chart(f_type="benzine", p_type="premium", min_scale=1.70, max_scale=2.40), use_container_width=True)
 
 st.markdown(body=STREAMLITTEXT.FUEL_COMMENT)
--- streamlit_site/data/charts.py
+++ streamlit_site/data/charts.py
@@ -11,17 +11,18 @@ class StreamlitCharts():
         self.db = PostgresHandler()
         self.db.initialize_conn()
     
-    def bgt_gas_chart(self):
+    def min_max_avg_chart(self, f_type: str, p_type: str, min_scale:float, max_scale:float):
         # Budget branstofpompen dataframe & chart benzine
-        bgt_dta = self.db.database_query(DB_QUERIES.price_query(fuel_type="benzine", pump_type="budget"))
-        bdt_g_df = pd.DataFrame(bgt_dta, columns=("Datum", "minimum price", "average price", "maximum price"))
-        bgt_g_price_cols = ["minimum price", "average price", "maximum price"]
+        dta = self.db.database_query(DB_QUERIES.price_query(fuel_type=f"{f_type}", pump_type=f"{p_type}"))
+        df = pd.DataFrame(dta, columns=("Datum", "minimum price", "average price", "maximum price"))
+        price_cols = ["minimum price", "average price", "maximum price"]
+        scale = [min_scale, max_scale]
 
         # Chart
-        bgt_g_chart = (
-            alt.Chart(bdt_g_df)
+        chart = (
+            alt.Chart(df)
             .transform_fold(
-                bgt_g_price_cols,
+                price_cols,
                 as_=["type", "price"]
             )
             .mark_line(point=True)
@@ -30,7 +31,7 @@ class StreamlitCharts():
                 y=alt.Y(
                     "price:Q",
                     title="Prijs (€)",
-                    scale=alt.Scale(domain=[1.65, 2.4]),
+                    scale=alt.Scale(domain=scale),
                     axis=alt.Axis(format=".3f")
                 ),
                 color=alt.Color("type:N", title="Prijssoort"),
@@ -41,103 +42,7 @@ class StreamlitCharts():
                 ]
             )
         )
-        return bgt_g_chart
-
-    def prem_gas_chart(self):
-        # Premium brandstofpompen dataframe & chart benzine
-        prem_g_dta = self.db.database_query(DB_QUERIES.price_query(fuel_type="benzine", pump_type="premium"))
-        prem_g_df = pd.DataFrame(prem_g_dta, columns=("Datum", "minimum price", "average price", "maximum price"))
-        prem_g_price_cols = ["minimum price", "average price", "maximum price"]
-
-        # Chart
-        prem_g_chart = (
-            alt.Chart(prem_g_df)
-            .transform_fold(
-                prem_g_price_cols,
-                as_=["type", "price"]
-            )
-            .mark_line(point=True)
-            .encode(
-                x=alt.X("Datum:T", title="Datum"),
-                y=alt.Y(
-                    "price:Q",
-                    title="Prijs (€)",
-                    scale=alt.Scale(domain=[1.65, 2.4]),
-                    axis=alt.Axis(format=".3f")
-                ),
-                color=alt.Color("type:N", title="Prijssoort"),
-                tooltip=[
-                    alt.Tooltip("Datum:T", title="Datum"),
-                    alt.Tooltip("type:N", title="Type"),
-                    alt.Tooltip("price:Q", title="Prijs (€)", format=".3f")
-                ]
-            )
-        )
-        return prem_g_chart
-
-    def bgt_diesel_chart(self):
-        # Budget branstofpompen dataframe & chart benzine
-        bgt_dta = self.db.database_query(DB_QUERIES.price_query(fuel_type="diesel", pump_type="budget"))
-        bgt_d_df = pd.DataFrame(bgt_dta, columns=("Datum", "minimum price", "average price", "maximum price"))
-        bgt_d_price_cols = ["minimum price", "average price", "maximum price"]
-
-        # Chart
-        bgt_d_chart = (
-            alt.Chart(bgt_d_df)
-            .transform_fold(
-                bgt_d_price_cols,
-                as_=["type", "price"]
-            )
-            .mark_line(point=True)
-            .encode(
-                x=alt.X("Datum:T", title="Datum"),
-                y=alt.Y(
-                    "price:Q",
-                    title="Prijs (€)",
-                    scale=alt.Scale(domain=[1.40, 2.1]),
-                    axis=alt.Axis(format=".3f")
-                ),
-                color=alt.Color("type:N", title="Prijssoort"),
-                tooltip=[
-                    alt.Tooltip("Datum:T", title="Datum"),
-                    alt.Tooltip("type:N", title="Type"),
-                    alt.Tooltip("price:Q", title="Prijs (€)", format=".3f")
-                ]
-            )
-        )
-        return bgt_d_chart
-
-    def prem_diesel_chart(self):
-        # Budget branstofpompen dataframe & chart benzine
-        prem_dta = self.db.database_query(DB_QUERIES.price_query(fuel_type="diesel", pump_type="premium"))
-        prem_d_df = pd.DataFrame(prem_dta, columns=("Datum", "minimum price", "average price", "maximum price"))
-        prem_d_price_cols = ["minimum price", "average price", "maximum price"]
-
-        # Chart
-        bgt_d_chart = (
-            alt.Chart(prem_d_df)
-            .transform_fold(
-                prem_d_price_cols,
-                as_=["type", "price"]
-            )
-            .mark_line(point=True)
-            .encode(
-                x=alt.X("Datum:T", title="Datum"),
-                y=alt.Y(
-                    "price:Q",
-                    title="Prijs (€)",
-                    scale=alt.Scale(domain=[1.40, 2.1]),
-                    axis=alt.Axis(format=".3f")
-                ),
-                color=alt.Color("type:N", title="Prijssoort"),
-                tooltip=[
-                    alt.Tooltip("Datum:T", title="Datum"),
-                    alt.Tooltip("type:N", title="Type"),
-                    alt.Tooltip("price:Q", title="Prijs (€)", format=".3f")
-                ]
-            )
-        )
-        return bgt_d_chart
+        return chart
     
     def type_chart(self):
         dta = self.db.database_query(DB_QUERIES.PUMP_TYPE_QUERY)
